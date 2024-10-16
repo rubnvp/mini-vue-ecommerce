fc53565d0d6776d69fcdd7e2023d85339ebc2895
rubnvp <rubn.vp@gmail.com>
2024-10-16 16:06:16 +0200

    link dev front and backend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+// import TheWelcome from '../components/TheWelcome.vue'
+const items = ref<unknown>([]);
+
+fetch('/api/grocery?_page=1&_per_page=20')
+  .then((res) => res.json())
+  .then((data) => {
+    items.value = data;
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    Groceries:
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        {{ item.name }}
+      </li>
+    </ul>
   </main>
 </template>
