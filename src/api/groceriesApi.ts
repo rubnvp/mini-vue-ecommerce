fdec25c78853c2795ed601e4eb7b7a4f579554f1
rubnvp <rubn.vp@gmail.com>
2024-11-12 18:03:25 +0100

    put cat images in json

--- src/api/groceriesApi.ts
+++ src/api/groceriesApi.ts
@@ -18,11 +18,7 @@ export async function fetchGroceries(
   });
   const response = await fetch(`${BASE_API}/grocery?${urlParams.toString()}`);
   const groceries = (await response.json()) as ItemType[];
-  // override the imageUrl with a random cat image (the original links are broken)
-  return groceries.map((grocery: ItemType) => ({
-    ...grocery,
-    imageUrl: getUrl(grocery.id),
-  }));
+  return groceries;
 }
 
 export async function updateGrocery(
@@ -37,32 +33,3 @@ export async function updateGrocery(
   });
   return response.json() as Promise<ItemType>;
 }
-
-function getUrl(id: string) {
-  // get a unique number base on the id
-  const index = parseInt(id, 36) % catImages.length;
-  return catImages[index];
-}
-
-const catImages = [
-  'https://cdn2.thecatapi.com/images/l7.jpg',
-  'https://cdn2.thecatapi.com/images/6an.jpg',
-  'https://cdn2.thecatapi.com/images/8cd.jpg',
-  'https://cdn2.thecatapi.com/images/a66.jpg',
-  'https://cdn2.thecatapi.com/images/bp9.jpg',
-  'https://cdn2.thecatapi.com/images/bps.jpg',
-  'https://cdn2.thecatapi.com/images/MTcxNTc1MA.jpg',
-  'https://cdn2.thecatapi.com/images/MTk3NTA0OA.jpg',
-  'https://cdn2.thecatapi.com/images/cw18Op1Ok.jpg',
-  'https://cdn2.thecatapi.com/images/_qUK20W2d.jpg',
-  'https://cdn2.thecatapi.com/images/8me.jpg',
-  'https://cdn2.thecatapi.com/images/8ot.jpg',
-  'https://cdn2.thecatapi.com/images/9p2.jpg',
-  'https://cdn2.thecatapi.com/images/aho.jpg',
-  'https://cdn2.thecatapi.com/images/aqp.jpg',
-  'https://cdn2.thecatapi.com/images/bf4.jpg',
-  'https://cdn2.thecatapi.com/images/bl8.jpg',
-  'https://cdn2.thecatapi.com/images/MjAzMDk0Mw.jpg',
-  'https://cdn2.thecatapi.com/images/MjAzOTQ2MA.jpg',
-  'https://cdn2.thecatapi.com/images/Zi4jfH3c6.jpg',
-];
