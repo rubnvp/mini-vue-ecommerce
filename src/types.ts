39433895dc03384e6beed3a31e6b421e29596014
rubnvp <rubn.vp@gmail.com>
2024-11-12 18:09:08 +0100

    workshop init

--- src/types.ts
+++
@@ -1,9 +0,0 @@
-export type ItemType = {
-  id: string;
-  imageUrl: string;
-  stock: number;
-  name: string;
-  price: number;
-  description: string;
-  favorite: boolean;
-};
--- src/utils.ts
+++
@@ -1,3 +0,0 @@
-export function pluralize(text: string, count: number, suffix = 's') {
-  return count === 1 ? text : text + suffix;
-}
